7c1140f9ccd1fae2122c6ea204cb993bbb0019f9
Janice <janice.ivena@ti.ukdw.ac.id>
2021-11-26 02:32:15 +0700

    Soal 3

---
+++ 3_A_71210812.py
@@ -0,0 +1,18 @@
+kalimat = input("Masukkan Kalimat: ")
+start = int(input("Index Start: "))
+stop = int(input("Index Stop: "))
+
+def hitung_hapus():
+    len(kalimat)
+    hapus = (stop - start)+ 1
+    hasil = (hapus/len(kalimat)) * 100
+    print(hasil)
+
+if start <= stop:
+    hitung_hapus()
+
+if start <= len(kalimat):
+    print(0.0)
+    
+else:
+    print(0.0)
